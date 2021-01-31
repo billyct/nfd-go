a9ef758d57feeeb5f078b03027d3989b29c79ca4
billyct <billy_allen@126.com>
2021-01-31 20:46:37 +0800

    feats: support OpenDialog, OpenDialogMultiple, SaveDialog

--- nfd.go
+++ nfd.go
@@ -35,6 +35,69 @@ import (
 	"unsafe"
 )
 
+func OpenDialog(filterList, defaultPath string) (res string, err error) {
+	cFilterList := cString(filterList)
+	cDefaultPath := cString(defaultPath)
+
+	var cOutPath *C.char
+	switch C.NFD_OpenDialog(cFilterList, cDefaultPath, &cOutPath) {
+	case C.NFD_OKAY:
+		res = C.GoString(cOutPath)
+		free(cOutPath)
+	case C.NFD_CANCEL:
+		// empty
+	default:
+		err = getError()
+	}
+
+	free(cDefaultPath)
+	free(cFilterList)
+	return
+}
+
+func OpenDialogMultiple(filterList, defaultPath string) (res []string, err error) {
+	cFilterList := cString(filterList)
+	cDefaultPath := cString(defaultPath)
+
+	var cOutPaths C.nfdpathset_t
+	switch C.NFD_OpenDialogMultiple(cFilterList, cDefaultPath, &cOutPaths) {
+	case C.NFD_OKAY:
+		var i C.size_t
+		for i = 0; i < C.NFD_PathSet_GetCount(&cOutPaths); i++ {
+			res = append(res, C.GoString(C.NFD_PathSet_GetPath(&cOutPaths, i)))
+		}
+		C.NFD_PathSet_Free(&cOutPaths)
+	case C.NFD_CANCEL:
+		// empty
+	default:
+		err = getError()
+	}
+
+	free(cDefaultPath)
+	free(cFilterList)
+	return
+}
+
+func SaveDialog(filterList, defaultPath string) (res string, err error) {
+	cFilterList := cString(filterList)
+	cDefaultPath := cString(defaultPath)
+
+	var cOutPath *C.char
+	switch C.NFD_SaveDialog(cFilterList, cDefaultPath, &cOutPath) {
+	case C.NFD_OKAY:
+		res = C.GoString(cOutPath)
+		free(cOutPath)
+	case C.NFD_CANCEL:
+		// empty
+	default:
+		err = getError()
+	}
+
+	free(cDefaultPath)
+	free(cFilterList)
+	return
+}
+
 func PickFolder(defaultPath string) (res string, err error)  {
 	cDefaultPath := cString(defaultPath)
 
